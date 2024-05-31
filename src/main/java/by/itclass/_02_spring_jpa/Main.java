775b700a72a0eae14a8243d3ef3926f9369e42cd
AlBogdanovich <alenuschkabogdanovich@gmail.com>
2024-05-31 15:19:52 +0300

    New Project

--- src/main/java/by/itclass/_02_spring_jpa/Main.java
+++ src/main/java/by/itclass/_02_spring_jpa/Main.java
@@ -2,13 +2,20 @@ package by.itclass._02_spring_jpa;
 
 import by.itclass._02_spring_jpa.config.AppConfig;
 import by.itclass._02_spring_jpa.entities.Airplane;
+import by.itclass._02_spring_jpa.entities.Passenger;
 import by.itclass._02_spring_jpa.repositories.AirplaneRepository;
+import by.itclass._02_spring_jpa.repositories.PassengerRepository;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import java.util.List;
+
+
 
 public class Main {
     public static void main(String[] args) {
         var ctx = new AnnotationConfigApplicationContext(AppConfig.class);
         var repository = ctx.getBean(AirplaneRepository.class);
+        var passRepository = ctx.getBean(PassengerRepository.class);
 
 //        var boeing736 = ctx.getBean("boeing736", Airplane.class);
 //        System.out.println("We just bought an airplane: " + boeing736);
@@ -20,10 +27,22 @@ public class Main {
 //        boeing736 = repository.save(boeing736);
 //        System.out.println("We changed places in boeing: " + boeing736);
 
-        System.out.println(repository.findByModel("AirBus"));
-        System.out.println(repository.findByModelAndPlaces("AirBus", 100));
-        System.out.println(repository.findByModelLike("%736"));
-        System.out.println("+++++++++++++++++++++++++++++++++++++++++++++++++");
-        repository.findByIdBetween(2, 10).forEach(System.out::println);
+//        System.out.println(repository.findByModel("AirBus"));
+//        System.out.println(repository.findByModelAndPlaces("AirBus", 100));
+//        System.out.println(repository.findByModelLike("%736"));
+//        System.out.println("+++++++++++++++++++++++++++++++++++++++++++++++++");
+//        repository.findByIdBetween(2, 10).forEach(System.out::println);
+
+        //       repository.selAllSamoliot().forEach(System.out::println);
+
+        //   System.out.println(repository.concreteSamoliot("Air%", 300));
+
+        //    repository.bigSamoliot().forEach(System.out::println);
+
+        var passengers = (List<Passenger>) ctx.getBean("passengers");
+        passRepository.saveAll(passengers);
+
+        var boeing= ctx.getBean("boeing736", Airplane.class);
+        repository.save(boeing);
     }
 }
--- src/main/java/by/itclass/_02_spring_jpa/repositories/AirplaneRepository.java
+++ src/main/java/by/itclass/_02_spring_jpa/repositories/AirplaneRepository.java
@@ -1,6 +1,7 @@
 package by.itclass._02_spring_jpa.repositories;
 
 import by.itclass._02_spring_jpa.entities.Airplane;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
@@ -15,4 +16,13 @@ public interface AirplaneRepository extends CrudRepository<Airplane, Integer> {
     Airplane findByModelAndPlaces(String model, int places);
     Airplane findByModelLike(String model);
     List<Airplane> findByIdBetween(int from, int to);
+
+    @Query(value = "SELECT * FROM airplane", nativeQuery = true)
+    List<Airplane> selAllSamoliot();
+
+    @Query(value = "SELECT * FROM airplane WHERE model LIKE ?1 AND places = ?2", nativeQuery = true)
+    Airplane concreteSamoliot(String model, int places);
+
+    @Query(value = "SELECT * FROM airplane WHERE places > 100", nativeQuery = true)
+    List<Airplane> bigSamoliot();
 }
